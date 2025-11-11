a3047a029aece5183339ead3d8e390c3405526fa
CorentinLeGuen <leguen.corentin@protonmail.com>
2025-11-11 10:09:39 -0500

    :recycle: cleanup project

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
 # :rose: Rose
 
-_async_ Rust webservices
+Rose (Rust Object storage Env) is a service based on `object_store` to keep track of space used.
 
 [![Project Status](https://img.shields.io/badge/status-in%20development-orange?style=for-the-badge)](https://github.com/CorentinLeGuen/nuage/tags)
 
 ## Run service
 
-Service should be available at [localhost:8003/health](localhost:8003/health)
+Health service is available at [localhost:8003/health](localhost:8003/health)
 
 ### Using cargo
 
 `cargo build` to build or `cargo run` to run the service
-
-### Using Docker
-
-Build `docker build -t rose .` and run `docker run -p 8003:8003 rose`
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,3 @@
-use std::net::SocketAddr;
-use axum::{
-    routing::get,
-    Json,
-    Router,
-};
-use serde::Serialize;
-
-#[derive(Serialize)]
-struct HealthResponse {
-    status: &'static str,
-}
-
-async fn health() -> Json<HealthResponse> {
-    Json(HealthResponse { status: "ok" })
-}
-
-#[tokio::main]
-async fn main() {
-    let app = Router::new().route("/health", get(health));
-
-    let addr: SocketAddr = "0.0.0.0:8003".parse().unwrap();
-    println!("Service running on {}", addr);
-
-    axum::serve(
-        tokio::net::TcpListener::bind(addr).await.unwrap(),
-        app
-    ).await.unwrap();
+fn main() {
+    println!("Hello, from 🌹.")
 }
